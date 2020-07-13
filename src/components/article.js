c05da4ce3e3f7738278f6285d0b10f60dadfa346
Tom Gehder <tom.gehder@leanix.net>
2020-07-13 13:46:48 +0200

    Adding laoding of independet html files

--- src/components/article.js
+++ src/components/article.js
@@ -2,14 +2,31 @@ import { LitElement, html, css } from 'https://unpkg.com/lit-element?module';
 
 export class Article extends LitElement {
   static get properties() {
-    return { uri: { type: String } };
+    return { uri: { type: String, hasChanged: this.uriChanged } };
   }
+
+  innerHTML = html``;
 
   static get styles() {
     return css``;
   }
 
+  firstUpdated() {
+    if (this.uri) {
+      this.loadHTMLLayout(this.uri);
+    }
+  }
+
+  async loadHTMLLayout(uri) {
+    const htmlFile = await (await fetch(uri)).text();
+    console.log(htmlFile);
+    const article = document.createElement('article');
+    article.innerHTML = htmlFile;
+    this.innerHTML = article;
+    this.requestUpdate();
+  }
+
   render() {
-    return html``;
+    return html`${this.innerHTML}`;
   }
 }
--- src/pages/home.page.js
+++ src/pages/home.page.js
@@ -2,6 +2,6 @@ import { LitElement, html } from 'https://unpkg.com/lit-element?module';
 
 export class HomePage extends LitElement {
   render() {
-    return html`<div>This is my side :) </div>`;
+    return html`<tg-article uri="src/templates/home.html"></tg-article>`;
   }
 }
